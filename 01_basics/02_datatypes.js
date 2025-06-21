46a3c9f23574ec8f52401a9625b89d0561a9d7f4
shivam <kushwahashivam56504@gmail.com>
2025-06-21 02:29:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs ,not browser
+
+console.log(3
+     + 
+    3 );//code readability  should be high
+
+    console.log("shivam");
+
+    let name = "shivam"
+    let age = 34
+    let isLoggedIn= false
+    let state;
+    
+
+    //number=>2 to powre 53
+    //bigint
+    //string =>""
+    // boolean => true/false
+    //null=> standlone value
+    //undefine =>
+    //symbole => unique
+
+    //object
+
+    console.log(typeof null);
+    console.log(typeof undefined);
+
+    
