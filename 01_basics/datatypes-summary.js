ce2d691a1119f460eab4c3051063ac1a9afe0ceb
shivam <kushwahashivam56504@gmail.com>
2025-06-23 08:55:37 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -33,3 +33,27 @@ const myFunction = function(){
 
 console.log(typeof myFunction);
 
+//****************************************************************************
+
+// stack(primitive), heap (non-primitive)
+
+let myYoutubename = "shivamkush@.com"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email:"user@google.com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+
+userTwo.email = "hitesh@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
